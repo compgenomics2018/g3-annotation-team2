63598ed3841f790f3daef40b2dacfe784ce7e6d1
Mathur_Shrey_smathur41 <smathur41@gatech.edu>
2018-04-07 16:20:39 -0400

    basic running of prokka for multiple files

---
+++ scripts/prokka.sh
@@ -0,0 +1,4 @@
+for xx in ~/*.fa; do
+        N=$(basename $xx .fa);
+        prokka --species Bacteria --norrna --notrna --force --centre X --compliant --locustag $N --outdir $N --prefix $N $xx;
+done
