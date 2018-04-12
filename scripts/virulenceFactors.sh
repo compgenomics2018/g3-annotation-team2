2b621b3758b8958202f18f95c836a8a3c7db04ee
pprabhu33 <pprabhu33@gatech.edu>
2018-04-12 12:20:51 -0400

    made minor changes to the code (name of scripts being called)

--- scripts/virulenceFactors.sh
+++ scripts/virulenceFactors.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 
-# perform BLAST
+# perform BLAST with database from victor and VFDB setB (nucleotide blast)
 /projects/data/Func_Annotation/blast_binaries/blastn -query /projects/data/Func_Annotation/Tools/final_assembled/assembled97.fasta -db finalGenes.fas -outfmt '6 qseqid qstart qend sseqid evalue sstart send sframe stitle qcovs pident' -max_hsps 1 -max_target_seqs 1 -evalue 1e-10 -out 97_vfdb.txt
 
 # call script to rearrange the columns
