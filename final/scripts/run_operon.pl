66120ab63bc640b1269bf8e11833d6392b445e24
Rong <rjin38@gatech.edu>
2018-04-12 21:11:30 -0400

    final update

--- final/scripts/run_operon.pl
+++ final/scripts/run_operon.pl
@@ -6,11 +6,11 @@ use strict;
 # this script will require the fasta file to build blast DB
 # this script will also require the operon table
 
-my $DB = "db.fasta"; #required in the current directory
-my $opr_table = "operon.opr"; #required in the current directory
+my $DB = "/projects/data/Func_Annotation/g3-annotation-team2/final/scripts/database/db.fasta"; #required
+my $opr_table = "/projects/data/Func_Annotation/g3-annotation-team2/final/scripts/database/operon.opr"; #required
 my $blast_threshold = 80;
 
-my $query = ""; #required in the command line
+my $query = "/projects/data/Func_Annotation/g3-annotation-team2/final/clusters/assembled97_proteins.faa"; #required
 
 
 #if ($ARGV[0]) {$opr_table = $ARGV[0];}
@@ -24,8 +24,8 @@ if ( ! -e $opr_table) {
 #if ($ARGV[1]) {$blast_file = $ARGV[1];}
 #else {print "need blast_file in the command line!\n"; exit 0;}
 
-if ($ARGV[0]) {$query = $ARGV[0];}
-else {print "need query file in the command line!\n"; exit 0;}
+#if ($ARGV[0]) {$query = $ARGV[0];}
+#else {print "need query file in the command line!\n"; exit 0;}
 
 if ( ! -e $query) {
 	print "can't find the file $query!\n"; exit 0;
@@ -52,9 +52,9 @@ while (<FILE>) {
 }
 close FILE;
 
-system "makeblastdb -in $DB -dbtype prot";
-my $blast_out = $query.".blast.out";
-system "blastp -db $DB -query $query -num_threads 4 -evalue 1e-10 -max_target_seqs 1 -max_hsps 1 -outfmt \"6 qseqid qstart qend sseqid evalue sstart send sframe stitle qcovs pident\" \> $blast_out";
+system "/projects/data/Func_Annotation/blast_binaries/makeblastdb -in $DB -dbtype prot";
+my $blast_out = "operon.blast.out";
+system "/projects/data/Func_Annotation/blast_binaries/blastp -db $DB -query $query -num_threads 4 -evalue 1e-10 -max_target_seqs 1 -max_hsps 1 -outfmt \"6 qseqid qstart qend sseqid evalue sstart send sframe stitle qcovs pident\" \> $blast_out";
 
 if ( ! -e $blast_out) {
 	print "no blast out file to process!\n"; exit 0;
@@ -76,7 +76,7 @@ while (<FILE>) {
 }
 
 
-my $final_output = $query.".out";
+my $final_output = $blast_out.".out";
 
 open FILE, ">$final_output";
 foreach (@output) {
@@ -84,4 +84,21 @@ foreach (@output) {
 }
 close FILE;
 
+
+# call script to rearrange the columns
+my $changecol_file = $final_output.".col";
+system "/projects/data/Func_Annotation/g3-annotation-team2/final/scripts/changecol_operons.pl $final_output $changecol_file";
+
+# call script to convert tabular form to gff
+my $operon_gff = $final_output.".operon.gff";
+system "/projects/data/Func_Annotation/g3-annotation-team2/final/scripts/converttogff_operons.py $changecol_file $operon_gff";
+
+# map the genes back to the original gff
+system "/projects/data/Func_Annotation/g3-annotation-team2/final/scripts/virulencemapper.pl -i $operon_gff -u /projects/data/Func_Annotation/g3-annotation-team2/final/clusters/assembled97_proteins.uc -t DOOR";
+
+print "FINALLY done with operon.gff";
+
 system "rm $blast_out";
+system "rm $final_output";
+system "rm $changecol_file";
+system "rm $operon_gff";
