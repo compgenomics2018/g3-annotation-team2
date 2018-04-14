18340d607edc21c4b5533e4fa64c912ea88cac35
Siu Lung Ng (Michael) <sng32@gatech.edu>
2018-04-14 16:54:29 -0400

    added phaster and rfam

--- final/pipeline.sh
+++ final/pipeline.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
-
+export PATH=$PATH:/projects/data/Func_Annotation/g3-annotation-team2/final/tools
+: '
 eggNOG_hmmer_database="bact"
 eggNOG_method="diamond"
-while getopts "d:t:" opt; do
+while getopts "fd:t:p:r:" opt; do
   case ${opt} in
     d ) if [ $OPTARG == "diamond" ]; then
 	    eggNOG_method="diamond"
@@ -20,7 +20,16 @@ while getopts "d:t:" opt; do
 	      eggNOG_hmmer_database=$OPTARG
 	      fi
       ;;
-    \? ) echo "Usage: cmd [-d eggNOG query method] [-t hmmer database_eggNOG]"
+    p )
+         path=$OPTARG     #PATH TO GENOMES(REQUIRE FOR PHASTER)
+      ;;
+    f )
+         f=1              #FAST MODE(SKIP PHASTER)
+      ;;
+    r )
+         ncRNA=$OPTARG    #PATH TO ncRNA PREDICTION RESULTS
+      ;;
+    \? ) echo "Usage: cmd [-d eggNOG query method] [-t hmmer database_eggNOG] [-f to skip Phaster] [-r to use ncRNA prediction results] [-p Genome Assembly SRR location]"
       ;;
   esac
 done
@@ -46,6 +55,19 @@ done
 #./tools/usearch -id 0.97 -cluster_fast ./tmp/sorted_by_length.fna -centroids ./clusters/assembled97_nucl.fna -uc ./clusters/assembled97_nucl.uc
 #./tools/usearch -id 1 -cluster_fast ./tmp/sorted_by_length.faa -centroids ./clusters/assembled100_proteins.faa -uc ./clusters/assembled100_proteins.uc
 #./tools/usearch -id 0.97 -cluster_fast ./tmp/sorted_by_length.faa -centroids ./clusters/assembled97_proteins.faa -uc ./clusters/assembled97_proteins.uc
+
+tm ./tmp/*
+rm ./tool_gff/DOOR/*.gff
+rm ./tool_gff/eggNOG/*.gff
+rm ./tool_gff/signalP/*.gff
+rm ./tool_gff/VFDB/*.gff
+rm ./tool_gff/rgi/*.gff
+rm ./tool_gff/rfam/*.gff
+rm ./tool_gff/Pilercr/*.gff
+rm ./tool_gff/phaster/*.gff
+rm ./tool_gff/lipoP/*.gff
+rm ./tool_gff/signalP/*.gff
+
 ###############################################################################
 #eggNOG-mapper
 egg_Name=$eggNOG_method
@@ -75,16 +97,27 @@ rm ./tool_gff/eggNOG/*
 ################################################################################
 #DOOR
 
-#./scripts/run_operon.pl ./assembled97_proteins.faa
 
+./scripts/run_operon.pl
 
 
 ###############################################################################
 #lipoP
 
+
+./scripts/lipop.sh
+
 ###############################################################################
 #phaster
 
+if [[ $f != 1 ]]; then
+    ls $path > tmp/genomeList.txt;
+    mkdir tmp/phaster;
+    mkdir tmp/phaster/phaster_result;
+
+    scripts/run_phaster.sh -g tmp/genomeList.txt -p $path -o tmp/phaster/;
+    scripts/phaster2gff.sh -g tmp/genomeList.txt -r tmp/phaster/phaster_result -o ../tool_gff/phaster;
+fi
 ###############################################################################
 #pilerCR
 
@@ -96,12 +129,24 @@ rm ./tool_gff/eggNOG/*
 
 ###############################################################################
 #RGI
+./scripts/run_rgi.py
+./scripts/mapper_proteins.pl -i ./tmp/rgi.gff -u ./clusters/assembled100_proteins.uc -t rgi
 
-#./scripts/run_rgi.py
-##ERROR: cant find blast+ [HELP]
+
+
+
 ###############################################################################
+signalP
+
+
+
+###############################################################################
+rfam
+rm ./tool_gff/rfam/*
+cp "$ncRNA"*.gff ./tool_gff/rfam/
 
 ###############################################################################
 #merge and sort results
 
 ./scripts/mergensort.py
+'
