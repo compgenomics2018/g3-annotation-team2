cd3527781f094ccec72acac8fc7170621a81b821
Brian <brianm@gatech.edu>
2018-04-14 18:16:33 -0400

    fixed pathing errors

--- final/scripts/run_pilercr.sh
+++ final/scripts/run_pilercr.sh
@@ -3,23 +3,27 @@
 #the input_directory should be the folder where contains assembly fasta results
 #output_directory will store output gff files
 #script_directory have script pilercr2gff.pl in it
+mkdir -p ./tmp/pilercr
 
 install_path="/projects/data/Func_Annotation/Tools/Non_coding/pilercr1.06"
 input_directory="/projects/data/Final_Results_AllGroups/Final_Assemblies"
-output_directory="/projects/data/Func_Annotation/g3-annotation-team2/final/tool_gff/Pilercr"
-tmp_directory="/projects/data/Func_Annotation/g3-annotation-team2/final/tmp/pilercr"
-script_directory="/projects/data/Func_Annotation/g3-annotation-team2/final/scripts"
+output_directory="./tool_gff/Pilercr"
+tmp_directory="./tmp/pilercr"
+script_directory="./scripts"
+
+
 
 #perfrom pilercr
 for f in $input_directory/*.fasta
 do
   f1=$(basename $f);
   f2=${f1%.fasta}.out;
-  $install_path/pilercr -in $f -out $tmp_directory/$f2 -noinfo -quiet;
+ # $install_path/pilercr -in $f -out $tmp_directory/$f2 -noinfo -quiet;
 done
 
+
 # call script to convert tabular form to gff
-for f in temp_directory/*.out
+for f in $tmp_directory/*.out
 do
   f1=$(basename $f);
   f2=${f1%.out}.gff;
